3afb8fce9119f973c5f43e2c4259d07eb27d59ae
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-03-07 00:53:09 +0600

    Added login validation and issue tracker page

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-console.log("script loaded!!");
+// console.log("script loaded!!");
+
+document.getElementById("sign-in-btn").addEventListener('click', function(){
+    console.log("sign in button clicked");
+    
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    console.log(username, password);
+    //match the credentials
+    if(username === "admin" && password === "admin123"){
+        window.location.assign("issues-tracker.html");
+    } else {
+        alert("Invalid username or password. Please try again.");
+    }
+})
